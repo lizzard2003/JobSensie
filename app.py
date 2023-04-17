c4cc5fefbf2361369ddb571c1779d8108921ae83
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-04-17 16:16:13 -0500

    commit3

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 # this app is made to as user questions about themselves to choose a careerpath
 # we are using flask
+
+
+from traitify import Traitify
+secret_key= "API_KEY"
+traitify= Traitify(secret_key)
+
+decks= traitify.get_decks()
+
+#set deck id
+traitify.deck_id = decks[0].id
+
+#create assessment
+assessment= traitify.create_assessment()
+
+#get assesment
